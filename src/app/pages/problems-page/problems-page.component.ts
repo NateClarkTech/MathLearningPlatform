0cc8689a90cfe0a29cb2ed6c105c3fef4001692d
NateClarkTech <nateclarktech@gmail.com>
2025-01-24 15:26:06 -0500

    added a tree that links to the problem types on problem page.

--- src/app/pages/problems-page/problems-page.component.ts
+++ src/app/pages/problems-page/problems-page.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface ProblemNode {
+  name: string;
+  routerLink?: string;
+  children?: ProblemNode[];
+}
+
+const TREE_DATA: ProblemNode[] = [
+  {
+    name: 'Set Theory',
+    children: [
+      {
+        name: 'Notation',
+        routerLink:"/set-theory",
+        children: [
+          {name: 'Set Membership', routerLink:"/problems/set-theory/notation/membership",},
+          {name: 'Subsets', routerLink:"/problems/set-theory/notation/subsets",},
+        ]
+      }, 
+    ]
+  },
+];
 
 @Component({
   selector: 'app-problems-page',
   imports: [
-    MatTreeModule
+    CommonModule,
+    RouterLink,
+    MatTreeModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './problems-page.component.html',
-  styleUrl: './problems-page.component.scss'
+  styleUrl: './problems-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProblemsPageComponent {
+  childrenAccessor = (node: ProblemNode) => node.children ?? [];
 
+  dataSource = TREE_DATA;
+
+  hasChild = (_: number, node: ProblemNode) => !!node.children && node.children.length > 0;
 }
